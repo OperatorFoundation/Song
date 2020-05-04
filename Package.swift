94105d5024b6aa515bac6136dbe29c08535ce60f
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2020-05-04 15:48:56 -0500

    Updated Datable version

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/yanagiba/swift-ast", from: "0.4.2"),
-        .package(url: "https://github.com/OperatorFoundation/Datable", from: "1.1.1"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
