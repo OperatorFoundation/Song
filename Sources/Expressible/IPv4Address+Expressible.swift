595e59fab79dac6a1e0f9d57a5691bb94e95beeb
Consuelita <mafalda@Mafaldas-MacBook-Pro.local>
2020-06-08 16:24:58 -0500

    Update IPv4Address+Expressible.swift

--- Sources/Expressible/IPv4Address+Expressible.swift
+++ Sources/Expressible/IPv4Address+Expressible.swift
@@ -4,28 +4,28 @@
 //
 //  Created by Mafalda on 5/26/20.
 //
-
-import Foundation
-import Network
-import AST
-
-extension IPv4Address: Expressible
-{
-    public var expression: Expression
-    {
-        let expression = LiteralExpression(kind: .staticString(ipString, ipStringQuoted))
-        let argument = FunctionCallExpression.Argument.expression(expression)
-        let identifier = Identifier.name("IPv4Address")
-        let postfixExpression = IdentifierExpression(kind: .identifier(identifier, nil))
-        let initializerExpression = InitializerExpression(postfixExpression: postfixExpression, argumentNames: [Identifier.name(ipStringQuoted)])
-        return initializerExpression
-    }
-    
-    public init?(expression: Expression)
-    {
-        guard let rawData = expression. as? Data
-            else { return nil }
-        self.init(rawData)
-    }
-}
+//
+//import Foundation
+//import Network
+//import AST
+//
+//extension IPv4Address: Expressible
+//{
+//    public var expression: Expression
+//    {
+//        let expression = LiteralExpression(kind: .staticString(ipString, ipStringQuoted))
+//        let argument = FunctionCallExpression.Argument.expression(expression)
+//        let identifier = Identifier.name("IPv4Address")
+//        let postfixExpression = IdentifierExpression(kind: .identifier(identifier, nil))
+//        let initializerExpression = InitializerExpression(postfixExpression: postfixExpression, argumentNames: [Identifier.name(ipStringQuoted)])
+//        return initializerExpression
+//    }
+//    
+//    public init?(expression: Expression)
+//    {
+//        guard let rawData = expression. as? Data
+//            else { return nil }
+//        self.init(rawData)
+//    }
+//}
 
