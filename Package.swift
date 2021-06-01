cd8ba924a7862c61cd8bf06154410d7e21a0381e
consuelita <adelita@operatorfoundation.org>
2021-06-01 14:09:10 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -40,8 +40,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/yanagiba/swift-ast", from: "0.4.3"),
-        .package(url: "https://github.com/OperatorFoundation/Datable", from: "3.0.2"),
+        .package(url: "https://github.com/OperatorFoundation/swift-ast.git", from: "0.4.3"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", from: "3.0.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
