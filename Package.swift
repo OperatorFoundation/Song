e186da626031d49db647c5b3d24c28adacc21c7c
consuelita <adelita@operatorfoundation.org>
2021-10-11 16:15:39 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 #if os(macOS)
 let package = Package(
     name: "Song",
-    platforms: [.macOS(.v11)],
+    platforms: [.macOS(.v10_14)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -40,8 +40,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/swift-ast.git", from: "0.4.3"),
-        .package(url: "https://github.com/OperatorFoundation/Datable", from: "3.0.4"),
+        .package(url: "https://github.com/OperatorFoundation/swift-ast.git", from: "0.19.11"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", from: "3.1.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
