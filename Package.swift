68145b93c775e1537df75a7af356890bcdbe30bf
CryptoSax <joshua@operatorfoundation.org>
2022-02-07 15:28:51 -0600

    changed package.swift to use branches

--- Package.swift
+++ Package.swift
@@ -39,9 +39,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/swift-ast.git", from: "0.19.11"),
-        .package(url: "https://github.com/OperatorFoundation/Datable", from: "3.1.2"),
-        .package(url: "https://github.com/OperatorFoundation/Net.git", from: "0.0.1"),
+        .package(url: "https://github.com/OperatorFoundation/swift-ast.git", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Net.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
