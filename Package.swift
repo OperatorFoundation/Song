b1207a69ce24a11f8e522a0c6fade37081c308b8
bragelbytes <joebragel@gmail.com>
2023-12-14 19:53:15 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -41,9 +41,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/swift-ast.git", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/swift-ast", branch: "main"),
         .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/Net.git", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Net", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
